52a8179da9a11e76a151a708d716a6fbca68722a
Jonathan Taylor <jonyonson@icloud.com>
2019-05-14 18:17:02 -0500

    changed typography

--- src/utils/typography.js
+++ src/utils/typography.js
@@ -1,23 +1,26 @@
-import Typography from "typography"
-import Wordpress2016 from "typography-theme-wordpress-2016"
+import Typography from 'typography';
+// import Wordpress2016 from "typography-theme-wordpress-2016"
+import funstonTheme from 'typography-theme-funston';
 
-Wordpress2016.overrideThemeStyles = () => {
+// Wordpress2016.overrideThemeStyles = () => {
+funstonTheme.overrideThemeStyles = () => {
   return {
-    "a.gatsby-resp-image-link": {
+    'a.gatsby-resp-image-link': {
       boxShadow: `none`,
     },
-  }
-}
+  };
+};
 
-delete Wordpress2016.googleFonts
+// delete Wordpress2016.googleFonts;
 
-const typography = new Typography(Wordpress2016)
+// const typography = new Typography(Wordpress2016);
+const typography = new Typography(funstonTheme);
 
 // Hot reload typography in development.
 if (process.env.NODE_ENV !== `production`) {
-  typography.injectStyles()
+  typography.injectStyles();
 }
 
-export default typography
-export const rhythm = typography.rhythm
-export const scale = typography.scale
+export default typography;
+export const rhythm = typography.rhythm;
+export const scale = typography.scale;
