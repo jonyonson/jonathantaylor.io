1028a8405ec1398fbc2fb611b49d1eaede98a438
Jonathan Taylor <jonyonson@icloud.com>
2019-06-17 17:47:19 -0500

    page transitions using react-transition-group

---
+++ src/layouts/index.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+import Header from '../components/header';
+import Footer from '../components/footer';
+import Transition from '../components/transition.js';
+import 'typeface-space-mono';
+import './layout.scss';
+
+import 'prismjs/themes/prism-solarizedlight.css';
+// import 'prismjs/plugins/line-numbers/prism-line-numbers.css';
+import '../styles/prism-styles.scss';
+
+class Layout extends Component {
+  render() {
+    const { children, location } = this.props;
+
+    return (
+      <div className="layout">
+        <Header />
+        <main className="layout-main">
+          <div className="layout-main__container">
+            <Transition location={location}>{children}</Transition>
+          </div>
+        </main>
+        <Footer />
+      </div>
+    );
+  }
+}
+
+export default Layout;
