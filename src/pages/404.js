58e9fd49c4fb651900302bdc7f140863f6ad1c17
Jonathan Taylor <jonyonson@icloud.com>
2019-06-17 18:50:19 -0500

    minor updates

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { graphql } from 'gatsby';
 
-import Layout from '../components/layout';
 import SEO from '../components/seo';
 
 class NotFoundPage extends React.Component {
@@ -10,11 +9,11 @@ class NotFoundPage extends React.Component {
     const siteTitle = data.site.siteMetadata.title;
 
     return (
-      <Layout location={this.props.location} title={siteTitle}>
+      <React.Fragment>
         <SEO title="404: Not Found" />
         <h1>Not Found</h1>
         <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
-      </Layout>
+      </React.Fragment>
     );
   }
 }
