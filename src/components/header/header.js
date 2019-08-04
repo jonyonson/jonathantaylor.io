420e8951693e9564d08f21cc61788e5c2cd4218d
Jonathan Taylor <jonyonson@icloud.com>
2019-08-04 18:10:17 -0500

    move projects to new branch

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { Link } from 'gatsby';
-// import { FiMenu } from 'react-icons/fi';
 import './header.scss';
 
 class Header extends React.Component {
@@ -13,15 +12,10 @@ class Header extends React.Component {
           </Link>
 
           <nav className="header__nav">
-            {/* <Link className="header__nav-link" to="/projects">
-              Projects
-            </Link> */}
             <Link className="header__nav-link" to="/contact">
               Contact
             </Link>
           </nav>
-
-          {/* <FiMenu className="header__menu" /> */}
         </div>
       </header>
     );
