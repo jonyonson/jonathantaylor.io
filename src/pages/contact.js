9df0f2888530e0427fb2b4ba48a5772cce62fd5f
Jonathan Taylor <jonyonson@icloud.com>
2019-08-12 15:50:47 -0500

    working contact form [WIP]

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -10,31 +10,43 @@ class Contact extends Component {
 
   handleSubmit = e => {
     e.preventDefault();
-    this.setState({
-      error:
-        "This form isn't functional just yet.  Try hitting me up on Twitter.",
-    });
+    const form = e.target;
   };
   render() {
     return (
       <React.Fragment>
         <SEO title="Contact Me" />
+
         <p className="contact-intro">
           Say hi, hire me, tell me a joke, suggest a TV show, compliment me,
           roast me or toast me.
         </p>
-        <form className="form" onSubmit={this.handleSubmit}>
+
+        <form
+          className="form"
+          name="contact"
+          method="post"
+          action="/thanks/"
+          data-netlify="true"
+          data-netlify-honeypot="bot-field"
+          // onSubmit={this.handleSubmit}
+        >
+          <input type="hidden" name="bot-field" />
+          <input type="hidden" name="form-name" value="contact" />
+
           <label htmlFor="name">Name</label>
           <input id="name" type="text" />
+
           <label htmlFor="email">Email</label>
           <input id="email" type="text" />
+
           <label htmlFor="message">Message</label>
           <textarea id="message" type="text" rows="8" />
           <button type="submit">Send Message</button>
+
           {this.state.error && (
             <div className="contact-error">
               <IoIosWarning className="contact-error-icon" />
-
               <div>{this.state.error}</div>
             </div>
           )}
