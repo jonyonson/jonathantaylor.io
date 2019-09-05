bde2aac12736d7ddd708658b6cf3856beb25b994
Jonathan Taylor <jonyonson@icloud.com>
2019-09-04 21:33:14 -0500

    cleanup forms

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,61 +1,8 @@
-import React, { Component } from 'react';
+import React from 'react';
 import SEO from '../components/seo';
-import { IoIosWarning } from 'react-icons/io';
 import '../styles/pages/contact.scss';
 import { navigate } from 'gatsby';
 
-// class Contact extends Component {
-//   state = {
-//     error: null,
-//   };
-
-//   handleSubmit = e => {
-//     e.preventDefault();
-//     const form = e.target;
-//   };
-//   render() {
-//     return (
-//       <React.Fragment>
-//         <SEO title="Contact Me" />
-
-//         <p className="contact-intro">
-//           Say hi, hire me, tell me a joke, suggest a TV show, compliment me,
-//           roast me or toast me.
-//         </p>
-
-//         <form
-//           className="form"
-//           name="contact"
-//           method="post"
-//           action="/thanks/"
-//           data-netlify="true"
-//           data-netlify-honeypot="bot-field"
-//           // onSubmit={this.handleSubmit}
-//         >
-//           <input type="hidden" name="bot-field" />
-//           <input type="hidden" name="form-name" value="contact" />
-
-//           <label htmlFor="name">Name</label>
-//           <input id="name" type="text" />
-
-//           <label htmlFor="email">Email</label>
-//           <input id="email" type="text" />
-
-//           <label htmlFor="message">Message</label>
-//           <textarea id="message" type="text" rows="8" />
-//           <button type="submit">Send Message</button>
-
-//           {this.state.error && (
-//             <div className="contact-error">
-//               <IoIosWarning className="contact-error-icon" />
-//               <div>{this.state.error}</div>
-//             </div>
-//           )}
-//         </form>
-//       </React.Fragment>
-//     );
-//   }
-// }
 function encode(data) {
   return Object.keys(data)
     .map(key => encodeURIComponent(key) + '=' + encodeURIComponent(data[key]))
@@ -85,15 +32,14 @@ function Contact() {
   };
 
   return (
-    <React.Fragment>
+    <>
       <SEO title="Contact Me" />
-
       <p className="contact-intro">
         Say hi, hire me, tell me a joke, suggest a TV show, compliment me, roast
         me or toast me.
       </p>
-
       <form
+        className="form"
         name="contact"
         method="post"
         action="/thanks/"
@@ -109,32 +55,18 @@ function Contact() {
             <input name="bot-field" onChange={handleChange} />
           </label>
         </p>
-        <p>
-          <label>
-            Your name:
-            <br />
-            <input type="text" name="name" onChange={handleChange} />
-          </label>
-        </p>
-        <p>
-          <label>
-            Your email:
-            <br />
-            <input type="email" name="email" onChange={handleChange} />
-          </label>
-        </p>
-        <p>
-          <label>
-            Message:
-            <br />
-            <textarea name="message" onChange={handleChange} />
-          </label>
-        </p>
-        <p>
-          <button type="submit">Send</button>
-        </p>
+        <label>Name</label>
+        <input type="text" name="name" onChange={handleChange} />
+
+        <label>Email</label>
+        <input type="email" name="email" onChange={handleChange} />
+
+        <label>Message</label>
+        <textarea name="message" type="text" rows="8" onChange={handleChange} />
+
+        <button type="submit">Send Message</button>
       </form>
-    </React.Fragment>
+    </>
   );
 }
 
