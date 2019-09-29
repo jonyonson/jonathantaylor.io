577d8601b35ad3bbb1b021ce9a9abc544898dd35
Jonathan Taylor <jonathan.taylor.dev@gmail.com>
2019-09-29 14:44:21 -0500

    add ga-tracking id

--- gatsby-config.js
+++ gatsby-config.js
@@ -55,7 +55,7 @@ module.exports = {
     {
       resolve: 'gatsby-plugin-google-analytics',
       options: {
-        trackingId: 'ADD YOUR TRACKING ID HERE',
+        trackingId: 'UA-69543893-1',
       },
     },
     'gatsby-plugin-feed',
