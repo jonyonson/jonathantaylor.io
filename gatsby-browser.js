f6d8cef46ceeaeca947435cdffeef8941efe6051
Jonathan Taylor <jonathan.taylor.dev@gmail.com>
2019-10-14 04:30:34 -0500

    move font imports from helmet to gatsby-browser

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,5 +1,6 @@
 // custom typefaces
-// import 'typeface-montserrat';
-// import 'typeface-merriweather';
+import 'typeface-space-mono';
+import 'typeface-quattrocento-sans';
+import 'typeface-work-sans';
 
 import './src/styles/_globals.css';
