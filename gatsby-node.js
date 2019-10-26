71d70fc4f325a67921bed6b8ff600d626c221b73
Jonathan Taylor <jonathan.taylor.dev@gmail.com>
2019-10-26 11:17:53 -0500

    code block styling

--- gatsby-node.js
+++ gatsby-node.js
@@ -39,12 +39,10 @@ exports.createPages = ({ graphql, actions }) => {
       const next = index === 0 ? null : posts[index - 1].node;
       console.log(post.node.fields.slug.split('--')[1]);
       createPage({
-        // path: post.node.fields.slug,
-        path: '/' + post.node.fields.slug.split('--')[1],
+        path: post.node.fields.slug,
         component: blogPost,
         context: {
-          // slug: post.node.fields.slug,
-          slug: '/' + post.node.fields.slug.split('--')[1],
+          slug: post.node.fields.slug,
           previous,
           next,
         },
