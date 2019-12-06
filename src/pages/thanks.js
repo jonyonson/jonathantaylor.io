be957d79043ce579457d11b52bcd295a3229f7f4
Jonathan Taylor <jonathan.taylor.dev@gmail.com>
2019-12-05 20:32:57 -0600

    fix linting error

--- src/pages/thanks.js
+++ src/pages/thanks.js
@@ -1,8 +1,7 @@
 import React from 'react';
 
-export default () => (
-  <div>
-    <h1>Thank you!</h1>
-    {/* <p>This is a custom thank you page for form submissions</p> */}
-  </div>
-);
+function FormSuccess() {
+  return <h1>Thank you!</h1>;
+}
+
+export default FormSuccess;
