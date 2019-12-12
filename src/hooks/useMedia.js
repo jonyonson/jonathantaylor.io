31818b1a9dd22adee31741ac73782735348eaa78
Jonathan Taylor <jonathan.taylor.dev@gmail.com>
2019-12-12 16:31:22 -0600

    revert undefined check

--- src/hooks/useMedia.js
+++ src/hooks/useMedia.js
@@ -2,19 +2,20 @@ import { useState, useEffect, useCallback } from 'react';
 
 export default function useMedia(queries, values, defaultValue) {
   // Array containing a media query list for each query
-  let mediaQueryLists;
-  if (typeof window !== 'undefined') {
-    mediaQueryLists = queries.map((q) => window.matchMedia(q));
-  }
+  const mediaQueryLists = queries.map((q) => window.matchMedia(q));
+
+  // let mediaQueryLists;
+  // if (typeof window !== 'undefined') {
+  //   mediaQueryLists = queries.map((q) => window.matchMedia(q));
+  // }
 
   // Function that gets value based on matching media query
   // wrapped into its own useCallback() hook to prevent change on every render
   const getValue = useCallback(() => {
     // Get index of first media query that matches
-    let index;
-    if (mediaQueryLists) {
-      index = mediaQueryLists.findIndex((mql) => mql.matches);
-    }
+
+    const index = mediaQueryLists.findIndex((mql) => mql.matches);
+
     // Return related value or defaultValue if none
     return typeof values[index] !== 'undefined' ? values[index] : defaultValue;
   }, [defaultValue, values, mediaQueryLists]);
