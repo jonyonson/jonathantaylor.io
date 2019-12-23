eec0bce61b280e2b1e32fb9d1ef9150eebca8f2f
Jonathan Taylor <jonathan.taylor.dev@gmail.com>
2019-12-22 19:50:17 -0600

    change node.internal.type to Mdx

--- gatsby-node.js
+++ gatsby-node.js
@@ -55,7 +55,7 @@ exports.createPages = ({ graphql, actions }) => {
 exports.onCreateNode = ({ node, actions, getNode }) => {
   const { createNodeField } = actions;
 
-  if (node.internal.type === 'MarkdownRemark') {
+  if (node.internal.type === 'Mdx') {
     const value = createFilePath({ node, getNode });
     createNodeField({
       name: 'slug',
