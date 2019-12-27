8a579d3579f8e03954bd6688fa0531d24e913b89
Jonathan Taylor <jonathan.taylor.dev@gmail.com>
2019-12-26 23:01:04 -0600

    prev & next links dark theme styles

---
+++ src/styles/styles.js
@@ -0,0 +1,6 @@
+const styles = {
+  bodyFontColor: 'hsla(0, 0%, 0%, 0.8)',
+  primaryColor: `#fb4d3d`,
+};
+
+export default styles;
--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -1,19 +1,16 @@
 import React from 'react';
 import { Link, graphql } from 'gatsby';
-import styled from 'styled-components';
-import SEO from '../components/seo';
-// import {
-//   TiArrowRightOutline as FaArrowRight,
-//   TiArrowLeftOutline as FaArrowLeft,
-// } from 'react-icons/ti';
 import { MDXRenderer } from 'gatsby-plugin-mdx';
+// import styled from 'styled-components';
+import SEO from '../components/seo';
 import Container from '../components/container';
+// import styles from '../styles/styles';
+import './blog-post.scss';
 
 class BlogPostTemplate extends React.Component {
   render() {
     const post = this.props.data.mdx;
     const { previous, next } = this.props.pageContext;
-    console.log(next, previous);
     return (
       <Container>
         <SEO
@@ -25,21 +22,8 @@ class BlogPostTemplate extends React.Component {
           {post.frontmatter.date}
         </p>
         <MDXRenderer>{post.body}</MDXRenderer>
-        {/* <div dangerouslySetInnerHTML={{ __html: post.html }} /> */}
         <hr />
-        {/* <ul
-          style={{
-            listStyle: 'none',
-            padding: 0,
-            margin: 0,
-            display: 'flex',
-            flexDirection: 'column',
-            alignItems: 'center',
-          }}
-        > */}
-
-        <PostLinks>
-          <PostLink exists={previous}>
+        {/* <PostLink exists={previous}>
             {previous && (
               <Link to={previous.fields.slug} rel="prev">
                 <div className="left">
@@ -48,9 +32,9 @@ class BlogPostTemplate extends React.Component {
                 </div>
               </Link>
             )}
-          </PostLink>
+          </PostLink> */}
 
-          <PostLink exists={next}>
+        {/* <PostLink exists={next}>
             {next && (
               <Link to={next.fields.slug} rel="next">
                 <div className="right">
@@ -59,8 +43,30 @@ class BlogPostTemplate extends React.Component {
                 </div>
               </Link>
             )}
-          </PostLink>
-        </PostLinks>
+          </PostLink> */}
+        <div className="post-links">
+          <div className="post-link">
+            {previous && (
+              <Link to={previous.fields.slug} rel="prev">
+                <div className="left">
+                  <p>Previous Article</p>
+                  <p>{previous.frontmatter.title}</p>
+                </div>
+              </Link>
+            )}
+          </div>
+
+          <div className="post-link">
+            {next && (
+              <Link to={next.fields.slug} rel="next">
+                <div className="right">
+                  <p>Next Article</p>
+                  <p>{next.frontmatter.title}</p>
+                </div>
+              </Link>
+            )}
+          </div>
+        </div>
       </Container>
     );
   }
@@ -89,44 +95,40 @@ export const pageQuery = graphql`
   }
 `;
 
-const PostLinks = styled.div`
-  display: flex;
-  margin-bottom: 1.45rem;
+// const PostLinks = styled.div`
+//   display: flex;
+//   margin-bottom: 1.45rem;
 
-  @media (max-width: $PHONE) {
-    flex-direction: column;
-  }
-`;
+//   @media (max-width: $PHONE) {
+//     flex-direction: column;
+//   }
+// `;
 
-const PostLink = styled.div`
-  width: 50%;
-  border: ${(props) => (props.exists ? '1px solid black' : 'none')};
+// const PostLink = styled.div`
+//   width: 50%;
+//   /* border: ${(props) => (props.exists ? '1px solid black' : 'none')}; */
 
-  &:first-of-type {
-    margin-right: 0.5rem;
-  }
+//   &:first-of-type { margin-right: 0.5rem;}
+//   &:last-of-type { margin-left: 0.5rem; }
+//   &:hover {
+//     /* background: ${(props) => (props.exists ? 'green' : 'none')}; */
+//   }
 
-  &:last-of-type {
-    margin-left: 0.5rem;
-  }
+//   div {
+//     padding: 0.5rem;
+//     &.right {
+//       text-align: right;
+//     }
 
-  &:hover {
-    /* background: ${(props) => (props.exists ? 'green' : 'none')}; */
-  }
+//     p {
+//       color: ${styles.bodyFontColor};
+//       font-size: 80%;
+//       margin-bottom: 0.5rem;
+//     }
 
-  div {
-    padding: 0.5rem;
-    &.right {
-      text-align: right;
-    }
-
-    p {
-      font-size: 80%;
-      margin-bottom: 0.5rem;
-    }
-
-    p:last-of-type {
-      margin: 0;
-    }
-  }
-`;
+//     p:last-of-type {
+//       margin: 0;
+//       color: ${styles.primaryColor}
+//     }
+//   }
+// `;
