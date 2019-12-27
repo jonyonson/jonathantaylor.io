5f680b8644ecf18ef320a9e45ef8e2d63913e288
Jonathan Taylor <jonathan.taylor.dev@gmail.com>
2019-12-26 22:21:05 -0600

    update previous and next link styling

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import { Link, graphql } from 'gatsby';
+import styled from 'styled-components';
 import SEO from '../components/seo';
-import {
-  TiArrowRightOutline as FaArrowRight,
-  TiArrowLeftOutline as FaArrowLeft,
-} from 'react-icons/ti';
+// import {
+//   TiArrowRightOutline as FaArrowRight,
+//   TiArrowLeftOutline as FaArrowLeft,
+// } from 'react-icons/ti';
 import { MDXRenderer } from 'gatsby-plugin-mdx';
 import Container from '../components/container';
 
@@ -12,7 +13,7 @@ class BlogPostTemplate extends React.Component {
   render() {
     const post = this.props.data.mdx;
     const { previous, next } = this.props.pageContext;
-
+    console.log(next, previous);
     return (
       <Container>
         <SEO
@@ -26,7 +27,7 @@ class BlogPostTemplate extends React.Component {
         <MDXRenderer>{post.body}</MDXRenderer>
         {/* <div dangerouslySetInnerHTML={{ __html: post.html }} /> */}
         <hr />
-        <ul
+        {/* <ul
           style={{
             listStyle: 'none',
             padding: 0,
@@ -35,35 +36,31 @@ class BlogPostTemplate extends React.Component {
             flexDirection: 'column',
             alignItems: 'center',
           }}
-        >
-          <li style={{ textAlign: 'center' }}>
+        > */}
+
+        <PostLinks>
+          <PostLink exists={previous}>
             {previous && (
               <Link to={previous.fields.slug} rel="prev">
-                {/* ← {previous.frontmatter.title} */}
-                <div style={{ display: 'flex', alignItems: 'center' }}>
-                  <FaArrowLeft style={{ marginRight: '0.3rem' }} />{' '}
-                  {previous.frontmatter.title}
+                <div className="left">
+                  <p>Previous Article</p>
+                  <p>{previous.frontmatter.title}</p>
                 </div>
               </Link>
             )}
-          </li>
-          <li style={{ textAlign: 'center' }}>
+          </PostLink>
+
+          <PostLink exists={next}>
             {next && (
               <Link to={next.fields.slug} rel="next">
-                {/* {next.frontmatter.title} → */}
-                <div
-                  style={{
-                    display: 'flex',
-                    alignItems: 'center',
-                  }}
-                >
-                  {next.frontmatter.title}{' '}
-                  <FaArrowRight style={{ marginLeft: '0.3rem' }} />
+                <div className="right">
+                  <p>Next Article</p>
+                  <p>{next.frontmatter.title}</p>
                 </div>
               </Link>
             )}
-          </li>
-        </ul>
+          </PostLink>
+        </PostLinks>
       </Container>
     );
   }
@@ -88,6 +85,48 @@ export const pageQuery = graphql`
         date(formatString: "MMMM D, YYYY")
         # description
       }
+    }
+  }
+`;
+
+const PostLinks = styled.div`
+  display: flex;
+  margin-bottom: 1.45rem;
+
+  @media (max-width: $PHONE) {
+    flex-direction: column;
+  }
+`;
+
+const PostLink = styled.div`
+  width: 50%;
+  border: ${(props) => (props.exists ? '1px solid black' : 'none')};
+
+  &:first-of-type {
+    margin-right: 0.5rem;
+  }
+
+  &:last-of-type {
+    margin-left: 0.5rem;
+  }
+
+  &:hover {
+    /* background: ${(props) => (props.exists ? 'green' : 'none')}; */
+  }
+
+  div {
+    padding: 0.5rem;
+    &.right {
+      text-align: right;
+    }
+
+    p {
+      font-size: 80%;
+      margin-bottom: 0.5rem;
+    }
+
+    p:last-of-type {
+      margin: 0;
     }
   }
 `;
