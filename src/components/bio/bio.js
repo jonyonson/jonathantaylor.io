41d5cfdee0b3bcaeff963e486f40f1657e2aa8e0
Jonathan Taylor <jonathan.taylor.dev@gmail.com>
2020-03-12 12:03:55 -0500

    add more bio

--- src/components/bio/bio.js
+++ src/components/bio/bio.js
@@ -8,6 +8,7 @@
 import React from 'react';
 import { StaticQuery, graphql } from 'gatsby';
 // import { Link } from 'gatsby';
+import Skills from '../skills';
 import Image from 'gatsby-image';
 import './bio.scss';
 
@@ -19,33 +20,40 @@ function Bio({ truncated }) {
       render={(data) => {
         const { author, social } = data.site.siteMetadata;
         return (
-          <div className="bio">
-            <Image
-              className="bio__avatar"
-              fixed={data.avatar.childImageSharp.fixed}
-              alt={author}
-            />
+          <>
+            <div className="bio">
+              <Image
+                className="bio__avatar"
+                fixed={data.avatar.childImageSharp.fixed}
+                alt={author}
+              />
 
-            <p>
-              <span className="bio__hand" role="img" aria-label="a waving hand">
-                👋🏻
-              </span>{' '}
-              <strong>Hi, I&#39;m Jonathan</strong>. I am a{' '}
-              <a href={`https://github.com/${social.github}`}>
-                software developer
-              </a>{' '}
-              that enjoys solving interesting problems and making cool things
-              for the web.{' '}
-              {/* {truncated && (
-                <>
-                  <Link to="/about">Read more...</Link>
-                  or take a
-                  look at some <Link to="/projects">projects</Link> I have been
-                  working on lately.
-                </>
-              )} */}
-            </p>
-          </div>
+              <p>
+                <span
+                  className="bio__hand"
+                  role="img"
+                  aria-label="a waving hand"
+                >
+                  👋🏻
+                </span>{' '}
+                <strong>Hi, I&#39;m Jonathan</strong>. I am a{' '}
+                <a href={`https://github.com/${social.github}`}>
+                  software developer
+                </a>{' '}
+                that enjoys solving interesting problems and making cool things
+                for the web.
+                {/* Take a look at some{' '}
+                <Link to="/projects">projects</Link> I have been working on
+                lately. */}
+                <p style={{ margin: '1rem 0 0' }}>
+                  I have experience working with various tools and technologies
+                  and I enjoy learning knew things, but these are some of my
+                  gotos.
+                </p>
+              </p>
+            </div>
+            <Skills />
+          </>
         );
       }}
     />
